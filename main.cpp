056d82a2198678f9959bb28ce2a232956a58cb97
John C Setterlund <clutchclayton806@users.noreply.github.com>
2023-04-03 20:50:07 -0500

    Changed Logic to include second example
    Commented out some code for previous example

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 //This is a single line comment
 
-int Add(int *pInt, int *pInt1);
+//int Add(int *pInt, int *pInt1);
+int* AddVal(int* pointInt, int* pointInt1, int* pointInt2);
 
 /*
  * This is a multi line comment
@@ -13,13 +14,29 @@ int main() {
     x=5, y=6;
     int* pntx = &x;
     int* pnty= &y;
+
+    /*
+    part 1 function call
     int sum = Add(pntx,pnty);
-    cout << sum;
+    */
+    int* pntz = {};
+    pntz = AddVal(pntx, pnty,pntz);
+    //cout << sum;
+    cout << *pntz;
     return 0;
 }
 
+/*
 int Add(int* pInt, int* pInt1) {
     int px = *pInt;
     int py = *pInt1;
     return px + py;
+}
+*/
+
+int* AddVal(int* pointInt, int* pointInt1, int* pointInt2){
+    int xpointInt = *pointInt; int ypointInt = *pointInt1;
+    int pz = xpointInt + ypointInt;
+    int* zpointer = &pz;
+    return zpointer;
 }
